c761c8432f608ab1a1e8eff5c10f16dedd03b203
Igor K <russdreamer@gmail.com>
2019-11-16 03:07:48 +0300

    version 2.2

--- src/main/java/com/toolittlespot/elements/PngElement.java
+++ src/main/java/com/toolittlespot/elements/PngElement.java
@@ -16,7 +16,7 @@ public class PngElement extends ImageElement {
     public boolean optimize(ApplicationArea application) {
         String[] processCommand = {
                 Constants.COMPRESSOR_PATH,
-                "--strip", "--speed", "1", "--force", "--output", //options
+                "--strip", "--speed", "1", "--force", "--skip-if-larger", "--output", //options
                 getTempFilePath(), getTempFilePath()};
 
         Process process;
